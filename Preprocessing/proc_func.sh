47ef28242301bfc33ed3912e2a1ff49b9358b889
Kai Hwang <kai.hwang@gmail.com>
2016-05-02 12:56:25 -0700

    check if directory exist.

--- Preprocessing/proc_func.sh
+++ Preprocessing/proc_func.sh
@@ -4,30 +4,32 @@ WD='/home/despoB/TRSEPPI/TTD'
 
 for s in 601; do
 
-	for site in FEF; do
+	for site in FEF MFG S1; do
 
-	cd ${WD}/${s}/${site}/${site}_run1
+		if [ -d ${WD}/${s}/${site}/${site}_run1 ]; then
 
-		if [ ! -e ${WD}/${s}/${site}/${site}_run1/nswktm_functional_4.nii.gz ]; then
-			
-			preprocessFunctional -dicom "IM*" \
-			-mprage_bet ${WD}/${s}/${site}/MPRAGE/mprage_bet.nii.gz \
-			-warpcoef ${WD}/${s}/${site}/MPRAGE/mprage_warpcoef.nii.gz \
-			-func_refimg ${WD}/${s}/${site}/${site}_run1_ref/*.dcm \
-			-tr 1.0 \
-			-rescaling_method 100_voxelmean \
-			-template_brain MNI_2mm \
-			-func_struc_dof bbr \
-			-warp_interpolation spline \
-			-constrain_to_template y \
-			-4d_slice_motion \
-			-no_hp \
-			-cleanup \
-			-custom_slice_times detect \
-			-delete_dicom archive \
-			-smoothing_kernel 4
+			cd ${WD}/${s}/${site}/${site}_run1
+
+			if [ ! -e ${WD}/${s}/${site}/${site}_run1/nswktm_functional_4.nii.gz ]; then
+				
+				preprocessFunctional -dicom "IM*" \
+				-mprage_bet ${WD}/${s}/${site}/MPRAGE/mprage_bet.nii.gz \
+				-warpcoef ${WD}/${s}/${site}/MPRAGE/mprage_warpcoef.nii.gz \
+				-func_refimg ${WD}/${s}/${site}/${site}_run1_ref/*.dcm \
+				-tr 1.0 \
+				-rescaling_method 100_voxelmean \
+				-template_brain MNI_2mm \
+				-func_struc_dof bbr \
+				-warp_interpolation spline \
+				-constrain_to_template y \
+				-4d_slice_motion \
+				-no_hp \
+				-cleanup \
+				-custom_slice_times detect \
+				-delete_dicom archive \
+				-smoothing_kernel 4
+			fi
 		fi
-	
 	done
 done
 
