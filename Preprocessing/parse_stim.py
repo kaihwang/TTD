d12ffa86c2390732b2870bc2498e0ca759515c6a
Kai Hwang <kai.hwang@gmail.com>
2017-07-22 13:49:16 -0700

    new parisng func.

--- Preprocessing/parse_stim.py
+++ Preprocessing/parse_stim.py
@@ -7,13 +7,15 @@ import numpy as np
 import os.path
 
 #parse block order
-os.chdir('/home/despoB/kaihwang/TRSE/TTD')
+#os.chdir('/home/despoB/kaihwang/TRSE/TTD')
 #Subjects = glob.glob('5*')
-Subjects = [601, 602, 603, 605]
-os.chdir('/home/despoB/kaihwang/TRSE/TTD/Scripts/Logs')
+#these are old FTTD subjects
+#[601, 602, 603, 605]
+#os.chdir('/home/despoB/kaihwang/TRSE/TTD/ScanLogs')
 
 
 def write_stimtime(filepath, inputvec):
+	''' short hand function to write AFNI style stimtime'''
 	if os.path.isfile(filepath) is False:
 			f = open(filepath, 'w')
 			for val in inputvec[0]:
@@ -26,105 +28,149 @@ def write_stimtime(filepath, inputvec):
 
 
 
-for s in Subjects:
+def parse_stim (subjects, ROIs, ntrials_per_run, num_runs):
+	''' parse stim log from pscyhtoolbox script
+	input: s, subjects
+		ROIs,
+		ntrials_per_rn
+		num_runs 
 
-	for ROI in ['F', 'M', 'S']:
+	'''
+	#os.chdir('/home/despoB/kaihwang/TRSE/TTD/ScanLogs')
+	for s in subjects:
+		for ROI in ROIs:
 
-		#fix issue with inconsistent naming
-		if ROI == 'F':
-			site = 'FEF'
-		if ROI == 'M':
-			site = 'MFG'
-		if ROI == 'S':
-			site = 'S1'		
+			#fix issue with inconsistent naming
+			if ROI == 'F':
+				site = 'FEF'
+			if ROI == 'M':
+				site = 'MFG'
+			if ROI == 'S':
+				site = 'S1'		
+			if ROI == 'Loc':
+				site = 'Loc'	
 
-		timing_logs = []
-		for ses in [3,2,1]:
-			filestring = 'fMRI_Data_%s_%s_session%s*.txt' %(s, ROI, ses)
-			timing_logs.append(glob.glob(filestring)[0]) #files to be loaded
+			filestring = '/home/despoB/kaihwang/TRSE/TTD/ScanLogs/fMRI_Data_%s_%s_ses*.txt' %(s, ROI)
+			timing_logs = sorted(glob.glob(filestring)) #important to sort since glob seems to randomly order files.
+			#to check the order is correct, in ascending order
+			print('check timing logs are in ascending order:')
+			print(timing_logs) 
 
-		print(timing_logs)
-		#load timing logs and concat into a pandas dataframe
-		df = pd.read_table(timing_logs[2], header= None).append(pd.read_table(timing_logs[1], header= None)).append(pd.read_table(timing_logs[0],header= None))
-		# give each column a name.
-		df.columns = ['SubjID', 'Condition', 'MotorMapping', 'Target', 'Accu', 'FA', 'RH', 'LH', 'RT', 'OnsetTime', 'pic']
+			#load timing logs, concat depend on number of sessions
+			df = pd.read_table(timing_logs[0], header= None)
+			for i in np.arange(1, len(timing_logs), 1):
+				df = df.append(pd.read_table(timing_logs[i], header= None))
+			# give each column a name.
+			df.columns = ['SubjID', 'Condition', 'MotorMapping', 'Target', 'Accu', 'FA', 'RH', 'LH', 'RT', 'OnsetTime', 'pic']
 
-		# create new column variable of "block number" for every trial (48 trials per run). 12 blocks totoal 
-		df['Block'] = np.repeat(np.arange(1, 13),48)
+			# create new column variable of "run number" for every trial (48 trials per run). 12 blocks totoal 
+			df['Run'] = np.repeat(np.arange(1, num_runs+1), ntrials_per_run)
 
-		# extract the order of each block condition and save to a text file
-		run_order = df.groupby(['Block', 'Condition', 'MotorMapping']).sum().reset_index()[['Block', 'Condition', 'MotorMapping']]
-		fn = '/home/despoB/TRSEPPI/TTD/Scripts/%s_%s_run_order' %(s, site)
-		if os.path.isfile(fn) is False:			
-			run_order[['Condition','MotorMapping']].to_csv(fn, index=None, header=None, )
+			# extract the order of each block condition and save to a text file
+			run_order = df.groupby(['Run', 'Condition', 'MotorMapping']).sum().reset_index()[['Run', 'Condition', 'MotorMapping']]
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_%s_run_order' %(s, site)
+			if os.path.isfile(fn) is False:			
+				run_order[['Condition','MotorMapping']].to_csv(fn, index=None, header=None, sep = '\t')
+
+			#write out target+distractor (TD) runs
+			TD_runs = run_order[(run_order['Condition'] == 'HF') | (run_order['Condition'] == 'FH')]['Run'].values	
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_TD_runs' %s
+			if os.path.isfile(fn) is False:			
+				np.savetxt(fn, TD_runs, fmt='%2d')
+
+			#write out 2bk runs
+			TD_runs = run_order[(run_order['Condition'] == 'H2') | (run_order['Condition'] == 'F2')]['Run'].values	
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_2B_runs' %s
+			if os.path.isfile(fn) is False:			
+				np.savetxt(fn, TD_runs, fmt='%2d')	
+
+			#write out passive runs	
+			To_runs = run_order[(run_order['Condition'] == 'Hp') | (run_order['Condition'] == 'Fp')]['Run'].values	
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_P_runs' %s
+			if os.path.isfile(fn) is False:			
+				np.savetxt(fn, To_runs, fmt='%2d')
+
+
+			#create FIR timing for every condition, extract trial timing for the first trial of every block
+			FH_stimtime = [['*']*num_runs] #stimtime format, * for runs with no events of this stimulus class
+			HF_stimtime = [['*']*num_runs] #create one of this for every condition
+			Fp_stimtime = [['*']*num_runs]
+			Hp_stimtime = [['*']*num_runs]
+			F2_stimtime = [['*']*num_runs]
+			H2_stimtime = [['*']*num_runs]
 		
+			for i, run in enumerate(np.arange(1,num_runs+1)):  #loop through 12 runs
+				run_df = df[df['Run']==run].reset_index() #"view" of block we are curreint sorting through
+				FH_run_trials = [] #empty vector to store trial time info for the current block
+				HF_run_trials = []
+				Hp_run_trials = []
+				Fp_run_trials = []
+				H2_run_trials = []
+				F2_run_trials = []
 
-		#write out target+distractor (TD) runs
-		TD_runs = run_order[(run_order['Condition'] == 'HF') | (run_order['Condition'] == 'FH')]['Block'].values	
-		fn = '/home/despoB/TRSEPPI/TTD/Scripts/%s_TD_runs' %s
-		if os.path.isfile(fn) is False:			
-			np.savetxt(fn, TD_runs, fmt='%2d')
-
-		#write out passive runs	
-		To_runs = run_order[(run_order['Condition'] == 'Hp') | (run_order['Condition'] == 'Fp')]['Block'].values	
-		fn = '/home/despoB/TRSEPPI/TTD/Scripts/%s_P_runs' %s
-		if os.path.isfile(fn) is False:			
-			np.savetxt(fn, To_runs, fmt='%2d')
-
-
-
-		#create FIR timing for every condition, extract trial timing for the first trial of every block
-		FH_stimtime = [['*']*12] #stimtime format, * for runs with no events of this stimulus class
-		HF_stimtime = [['*']*12] #create one of this for every condition
-		Fp_stimtime = [['*']*12]
-		Hp_stimtime = [['*']*12]
-	
-		for i, block in enumerate(np.arange(1,13)):  #loop through 12 locks
-			block_df = df[df['Block']==block].reset_index() #"view" of block we are curreint sorting through
-			FH_block_trials = [] #empty vector to store trial time info for the current block
-			HF_block_trials = []
-			Hp_block_trials = []
-			Fp_block_trials = []
-
-			for tr in np.arange(0,48,12):  #this is the first trial of each block
-				if block_df.loc[tr,'Condition'] in ('FH'):
-					FH_block_trials.append(block_df.loc[tr,'OnsetTime']) #if a match of condition, append trial timing						
-				
-				if block_df.loc[tr,'Condition'] in ('HF'):
-					HF_block_trials.append(block_df.loc[tr,'OnsetTime']) 
+				for tr in np.arange(0,ntrials_per_run):  #this is to loop through trials
+					if run_df.loc[tr,'Condition'] in ('FH'):
+						FH_run_trials.append(run_df.loc[tr,'OnsetTime']) #if a match of condition, append trial timing						
 					
-				if block_df.loc[tr,'Condition'] in ('Fp'):
-					Fp_block_trials.append(block_df.loc[tr,'OnsetTime']) 
+					if run_df.loc[tr,'Condition'] in ('HF'):
+						HF_run_trials.append(run_df.loc[tr,'OnsetTime']) 
+						
+					if run_df.loc[tr,'Condition'] in ('Fp'):
+						Fp_run_trials.append(run_df.loc[tr,'OnsetTime']) 
 
-				if block_df.loc[tr,'Condition'] in ('Hp'):
-					Hp_block_trials.append(block_df.loc[tr,'OnsetTime']) 		
+					if run_df.loc[tr,'Condition'] in ('Hp'):
+						Hp_run_trials.append(run_df.loc[tr,'OnsetTime']) 	
 
-			if any(FH_block_trials):
-				FH_stimtime[0][i] = FH_block_trials	#put trial timing of each block into the stimtime array	
-			if any(HF_block_trials):
-				HF_stimtime[0][i] = HF_block_trials			
-			if any(Fp_block_trials):
-				Fp_stimtime[0][i] = Fp_block_trials				
-			if any(Hp_block_trials):
-				Hp_stimtime[0][i] = Hp_block_trials
+					if run_df.loc[tr,'Condition'] in ('F2'):
+						F2_run_trials.append(run_df.loc[tr,'OnsetTime']) 
 
-		#write out stimtime array to text file. 		
-		fn = '/home/despoB/TRSEPPI/TTD/Scripts/%s_%s_FH_stimtime.1D' %(s, site)
-		write_stimtime(fn, FH_stimtime)		
-			
-		fn = '/home/despoB/TRSEPPI/TTD/Scripts/%s_%s_HF_stimtime.1D' %(s, site)
-		write_stimtime(fn, HF_stimtime)	
+					if run_df.loc[tr,'Condition'] in ('H2'):
+						H2_run_trials.append(run_df.loc[tr,'OnsetTime']) 				
 
-		fn = '/home/despoB/TRSEPPI/TTD/Scripts/%s_%s_Fp_stimtime.1D' %(s, site)
-		write_stimtime(fn, Fp_stimtime)	
-
-		fn = '/home/despoB/TRSEPPI/TTD/Scripts/%s_%s_Hp_stimtime.1D' %(s, site)
-		write_stimtime(fn, Hp_stimtime)					
+				if any(FH_run_trials):
+					FH_stimtime[0][i] = FH_run_trials	#put trial timing of each block into the stimtime array	
+				if any(HF_run_trials):
+					HF_stimtime[0][i] = HF_run_trials			
+				if any(Fp_run_trials):
+					Fp_stimtime[0][i] = Fp_run_trials				
+				if any(Hp_run_trials):
+					Hp_stimtime[0][i] = Hp_run_trials
+				if any(F2_run_trials):
+					F2_stimtime[0][i] = F2_run_trials				
+				if any(H2_run_trials):
+					H2_stimtime[0][i] = H2_run_trials	
 
 
+			#write out stimtime array to text file. 		
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_%s_FH_stimtime.1D' %(s, site)
+			write_stimtime(fn, FH_stimtime)		
+				
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_%s_HF_stimtime.1D' %(s, site)
+			write_stimtime(fn, HF_stimtime)	
+
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_%s_Fp_stimtime.1D' %(s, site)
+			write_stimtime(fn, Fp_stimtime)	
+
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_%s_Hp_stimtime.1D' %(s, site)
+			write_stimtime(fn, Hp_stimtime)	
+
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_%s_F2_stimtime.1D' %(s, site)
+			write_stimtime(fn, F2_stimtime)	
+
+			fn = '/home/despoB/TRSEPPI/TTD/ScanLogs/%s_%s_H2_stimtime.1D' %(s, site)
+			write_stimtime(fn, H2_stimtime)					
+
+if __name__ == "__main__":
+
+	Subjects = [7002]
+	ROIs = ['Loc']
+	ntrials_per_run = 39
+	nruns = 12
+
+	parse_stim (Subjects, ROIs, ntrials_per_run, nruns)
 
 #create FIR regressors #not used??
-# block_start_time = np.tile(([1.5, 42, 82.5, 121.5]),[4,1])
+# run_start_time = np.tile(([1.5, 42, 82.5, 121.5]),[4,1])
 # for tr in np.arange(0,18):
 #     StimTime = block_start_time + tr * 1.5
 #     g = tr+1
