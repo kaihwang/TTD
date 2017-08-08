38c6df756dd9da8e03af075d030c28e9aac4d422
Kai Hwang <kai.hwang@gmail.com>
2017-08-08 11:17:31 -0700

    prevent rerun.

--- Preprocessing/organize_BIDS.sh
+++ Preprocessing/organize_BIDS.sh
@@ -6,16 +6,18 @@ WD='/home/despoB/TRSEPPI/TTD'
 
 for s in 7004; do
 
-	for ses in Loc; do
-		#first use no converter, get dicom info
-		heudiconv -d ${WD}/Raw/{subject}_{session}/*/*/* -s ${s} -ss ${ses} \
-		-f /home/despoB/kaihwang/bin/TTD/Preprocessing/TTD_heuristics.py -c none \
-		-o ${WD}/BIDS --bids
+	for ses in Loc S1 IPS; do
 
-		#convert dicom
-		heudiconv -d ${WD}/Raw/{subject}_{session}/*/*/* -s ${s} -ss ${ses} \
-		-f /home/despoB/kaihwang/bin/TTD/Preprocessing/TTD_heuristics.py -c dcm2niix \
-		-o ${WD}/BIDS --bids --minmeta
+		if [ -d ${WD}/Raw/{subject}_{session} ]; then
+			#first use no converter, get dicom info
+			heudiconv -d ${WD}/Raw/{subject}_{session}/*/*/* -s ${s} -ss ${ses} \
+			-f /home/despoB/kaihwang/bin/TTD/Preprocessing/TTD_heuristics.py -c none \
+			-o ${WD}/BIDS --bids
 
+			#convert dicom
+			heudiconv -d ${WD}/Raw/{subject}_{session}/*/*/* -s ${s} -ss ${ses} \
+			-f /home/despoB/kaihwang/bin/TTD/Preprocessing/TTD_heuristics.py -c dcm2niix \
+			-o ${WD}/BIDS --bids --minmeta
+		fi
 	done
 done
